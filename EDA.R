ce844eede2ae7b1a7063703745a026d61dd782d2
RishiDarkDevil <rishi8001100192@gmail.com>
2022-05-04 00:55:07 +0530

    Complete stacked bar plots for response.

--- EDA.R
+++ EDA.R
@@ -6,6 +6,7 @@ library(GGally)
 library(psych)
 library(grid)
 library(gridExtra)
+library(viridis)
 
 # Summary of the data
 desc_summ <- describe(data[,3:ncol(data)])
@@ -26,16 +27,48 @@ data_long_rev$Resp <- factor(data_long_rev$Resp, levels = rev(levels))
 data_long_rev$Resp
 
 # Making Stacked Bar Plots for Grouped Qsns and Response
-Qsn_Name <- c("^REC.", "^SA.", "^PC.", "^SS.", "^ER.", "^ES.", "^RES.", "^CON.")
-
-stacked_bar_plot <- list()
-for (i in seq_along(Qsn_Name)) {
-  stacked_bar_plot[[i]] <- data_long %>%
-    filter(str_detect(Qsn, Qsn_Name[i])) %>%
-    ggplot() +
-    geom_bar(aes(Qsn, fill = Resp), position = "fill") +
-    coord_flip()
+gen_stacked_barplots <- function(plot_data = data_long) {
+  Qsn_Name <- c("^SA.", "^REC.", "^PC.", "^SS.", "^ER.", "^ES.", "^RES.", "^CON.")
+  
+  stacked_bar_plot <- list()
+  for (i in seq_along(Qsn_Name)) {
+    stacked_bar_plot[[i]] <- plot_data %>%
+      filter(str_detect(Qsn, Qsn_Name[i])) %>%
+      ggplot() +
+      geom_bar(aes(Qsn, fill = Resp), position = "fill")  +
+      labs(
+        x = NULL,
+        y = NULL
+      ) +
+      theme_bw() +
+      theme(
+        panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"),
+        strip.background = element_blank(),
+        legend.position = "none"
+      ) +
+      scale_fill_viridis(discrete = TRUE) +
+      coord_flip()
+  }
+  
+  stacked_bar_plot <- ggarrange(
+    plotlist = stacked_bar_plot, 
+    ncol = 2, nrow = 4, 
+    common.legend = TRUE, legend = "bottom"
+  )
+  
+  annotate_figure(
+    stacked_bar_plot,
+    left = text_grob("Question Codes", rot = 90),
+    bottom = text_grob("Proportion"),
+    top = text_grob("Response Proportions for Each Question")
+  )
 }
 
-ggarrange(plotlist = stacked_bar_plot, ncol = 2, nrow = 4)
-  
+# Generate Stacked bar plots for non-reverse scored questions
+gen_stacked_barplots()
+
+# Generate Stacked bar plots for reverse scored questions
+gen_stacked_barplots(data_long_rev)
