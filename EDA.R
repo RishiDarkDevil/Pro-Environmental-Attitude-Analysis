7f45738dad0c2f7bc53f33a4dec13f25b8314cb2
RishiDarkDevil <rishi8001100192@gmail.com>
2022-05-04 11:52:48 +0530

    Make Pair Plots and Correlation Plots

--- EDA.R
+++ EDA.R
@@ -3,14 +3,20 @@ library(forcats)
 library(stringr)
 library(ggpubr)
 library(GGally)
+library(ggcorrplot)
 library(psych)
 library(grid)
 library(gridExtra)
+library(plotly)
 library(viridis)
 
 # Summary of the data
 desc_summ <- describe(data[,3:ncol(data)])
 desc_summ
+
+# Summary of the net_score data
+desc_score_summ <- describe(data_net_score[,3:ncol(data_net_score)])
+desc_score_summ
 
 # Converting Data from Wide to Long Form
 # Factoring the non-reverse score responses
@@ -27,9 +33,10 @@ data_long_rev$Resp <- factor(data_long_rev$Resp, levels = rev(levels))
 data_long_rev$Resp
 
 # Making Stacked Bar Plots for Grouped Qsns and Response
+Qsn_Name <- c("^SA.", "^REC.", "^PC.", "^SS.", "^ER.", "^ES.", "^RES.", "^CON.")
+Plot_Nmaes <- c("Recycling", "Environmental Safety", "Percieved Control", "Social Support", "Environmental Reductionism", "Environmental Sensitivity", "Reuse", "Conservation")
+
 gen_stacked_barplots <- function(plot_data = data_long) {
-  Qsn_Name <- c("^SA.", "^REC.", "^PC.", "^SS.", "^ER.", "^ES.", "^RES.", "^CON.")
-  
   stacked_bar_plot <- list()
   for (i in seq_along(Qsn_Name)) {
     stacked_bar_plot[[i]] <- plot_data %>%
@@ -41,6 +48,9 @@ gen_stacked_barplots <- function(plot_data = data_long) {
         y = NULL
       ) +
       theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),strip.background = element_blank()) +
+      ggtitle(Plot_Nmaes[[i]]) +
       theme(
         panel.border = element_blank(),
         panel.grid.major = element_blank(),
@@ -63,7 +73,7 @@ gen_stacked_barplots <- function(plot_data = data_long) {
     stacked_bar_plot,
     left = text_grob("Question Codes", rot = 90),
     bottom = text_grob("Proportion"),
-    top = text_grob("Response Proportions for Each Question")
+    top = text_grob("Response Proportions for Each Question", face = "bold", size = 16)
   )
 }
 
@@ -72,3 +82,48 @@ gen_stacked_barplots()
 
 # Generate Stacked bar plots for reverse scored questions
 gen_stacked_barplots(data_long_rev)
+
+# Generate Pair Plots for Net Score of Items
+my_fn <- function(data, mapping, ...){
+  p <- ggplot(data = data, mapping = mapping) + 
+    geom_point() + 
+    geom_smooth(method=lm, formula = y~x, size = 1, se = FALSE, ...)
+  p
+}
+
+# Colored on the basis of Sex
+data_net_score %>%
+           mutate(Sex = as.factor(Sex)) %>%
+           ggpairs(columns = c("REC", "SA", "PC", "SS", "ER", "ES", "RES", "CON"), aes(color = Sex), lower = list(continuous = my_fn)) +
+           scale_color_viridis(discrete = TRUE) +
+           theme_bw() +
+           theme(plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(),
+                 panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),strip.background = element_blank()) +
+           ggtitle("Scatterplot Matrix for Score of Items")
+
+# Tile Plots for correlation
+Qsn_Name_Cont <- c("REC", "SA", "PC", "SS", "ER", "ES", "RES", "CON")
+
+
+corrplots <- list()
+for (i in seq_along(Qsn_Name_Cont)) {
+  corrplots[[i]] <- data_enc %>%
+    select(contains(Qsn_Name_Cont[[i]])) %>%
+    ggcorr(label = TRUE) +
+    scale_fill_viridis() +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),strip.background = element_blank(), panel.border = element_blank(), line = element_blank()) +
+    ggtitle(Plot_Nmaes[[i]])
+    
+}
+
+corrplots <- ggarrange(
+  plotlist = corrplots, 
+  ncol = 2, nrow = 4
+)
+
+annotate_figure(
+  corrplots,
+  top = text_grob("Correlation Plots", face = "bold", size = 16)
+)
