dcaeb31b9f0c9d664c66fe1c203f00ae0256798b
RishiDarkDevil <rishi8001100192@gmail.com>
2022-05-07 13:07:51 +0530

    continue modeling make domain on domain MLR

--- modeling.R
+++ modeling.R
@@ -1,5 +1,95 @@
 library(jtools)
 
+present_R.2 <- function(mods, table_theme = "mBlue", footnote = "Model R-squared", visual = TRUE) {
+  mods_summ <- mods %>%
+    map(~summary(.))
+  R.squared <<- tibble("Model" = Qsn_Name_Cont,  
+                      "R_square" = round(mods_summ %>%
+                                            map_dbl(~.$r.squared), digits = 2), 
+                      "Adj_R_square" = round(mods_summ %>%
+                                                map_dbl(~.$adj.r.squared), digits = 2))
+  if(!visual){
+  p <- ggtexttable(R.squared, theme = ttheme(table_theme), rows = NULL) %>%
+    tab_add_footnote(text = footnote, size = 10, face = "italic")}
+  else{
+    p <- R.squared %>%
+      gather(R_square, Adj_R_square, key = "Metric", value = "value") %>%
+      mutate(Model = as.factor(Model)) %>%
+      ggplot(aes(fct_reorder(Model, value), value, color = Metric, group = Metric)) +
+      geom_line(size = 1.5) +
+      geom_point(size = 2.5) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),strip.background = element_blank()) +
+      ggtitle("Model Performance") +
+      theme(
+        panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"),
+        strip.background = element_blank()
+      ) + 
+      labs(
+        x = "Model",
+        y = "Metric Value"
+      ) + 
+      scale_color_viridis(discrete = TRUE)
+  }
+  return(p)
+}
+
+present_ssr <- function(mods, table_theme = "mBlue", footnote = "Model R-squared", visual = TRUE) {
+  mods_summ <- mods %>%
+    map(~summary(.))
+  SSR <<- tibble("Model" = Qsn_Name_Cont,  
+                       "SSR" = round(mods_summ %>%
+                                            map_dbl(~sum(.$residuals^2)/.$df[2]), digits = 2))
+  if(!visual){
+    p <- ggtexttable(SSR, theme = ttheme(table_theme), rows = NULL) %>%
+      tab_add_footnote(text = footnote, size = 10, face = "italic")}
+  else{
+    p <- SSR %>%
+      mutate(Model = as.factor(Model)) %>%
+      ggplot(aes(fct_reorder(Model, SSR, .desc = TRUE), SSR, group = 1)) +
+      geom_line(size = 1.5) +
+      geom_point(size = 2.5) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),strip.background = element_blank()) +
+      ggtitle("Model Residuals") +
+      theme(
+        panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"),
+        strip.background = element_blank()
+      ) + 
+      labs(
+        x = "Model",
+        y = "Sum of Squares Residuals"
+      )
+  }
+  return(p)
+}
+
+format_reg_table <- function(model, digs = 2){
+  mod <- tidy(model, conf.level = .99, conf.int = TRUE) %>%
+    mutate(across(where(is.numeric), ~ round(., digits = digs)))
+  mod <- mod %>%
+    mutate(estimate = as.character(estimate)) %>%
+    mutate(estimate = paste(paste(estimate, ifelse(p.value < 0.05, "*",""), ifelse(p.value < 0.01, "*",""), ifelse(p.value < 0.001, "*",""), sep = ""), paste("[", conf.low, ", ", conf.high, "]", sep = ""), sep = "\n"))
+  mod <- mod %>%
+    select(term, estimate)
+  return(mod)
+}
+
+present_reg_mod <- function(model, title = "", table_theme = "mOrange") {
+  p <- ggtexttable(format_reg_table(model), theme = ttheme(table_theme), rows = NULL) %>%
+    tab_add_title(text = title, face = "bold", padding = unit(0.1, "line")) %>%
+    tab_add_footnote(text = paste("*** p < 0.001; ** p < 0.01;", " * p < 0.05; 99% C.I.", sep = "\n"), size = 10, face = "italic")
+  return(p)
+}
+
 # Linear Model Dependence between the Categories
 lin_mod_cats <- Qsn_Name_Cont %>%
   map(~str_c(.,"~.")) %>%
@@ -11,20 +101,48 @@ sig_lin_mod_cats <- lin_mod_cats %>%
 sig_lin_mod_cats_summ <- sig_lin_mod_cats %>%
   map(~summary(.))
 
-
-plot_summs(
+sig_lin_mod_cats_plot <- plot_summs(
   sig_lin_mod_cats,
-  ci_level = .99,
+  inner_ci_level = .95,
   omit.coefs = "(Intercept)",
   model.names = Qsn_Name_Cont, 
   colors = "Rainbow",
   point.size = 6
 )  +
-  theme(plot.title = element_text(size = 16, hjust = 0.5), panel.grid.major = element_blank(),
+  theme(plot.title = element_text(size = 14, hjust = 0.5), panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),strip.background = element_blank(),
-        plot.subtitle = element_text(size=13, hjust=0.5, face="italic", color="black")) +
-  ggtitle("Linear Model for Dependence of Each Category Score on All Other") +
-  labs(subtitle = "Multiple Linear Regression of Each Category Scores on All other Category Scores, including only the significant variables(By both forward and backward model selection)")
+        plot.subtitle = element_text(size=12, hjust=0.5, face="italic", color="black")) +
+  ggtitle("Linear Model Coefficients") +
+  labs(subtitle = "Multiple Linear Regression Coefficients, including only the significant variables(By both forward and backward model selection)")
+
+# model_present_list <- list()
+# for (i in seq_along(Qsn_Name_Cont)) {
+#   model_present_list[[i]] <- present_reg_mod(sig_lin_mod_cats[[i]], str_c("Model ", Qsn_Name_Cont[i]))
+# }
+
+model_info_table <- sig_lin_mod_cats %>%
+  map(~format_reg_table(.)) %>%
+  reduce(full_join, by = c('term'))
+model_info_table <- model_info_table[2:nrow(model_info_table),]
+model_info_table[is.na(model_info_table)] <- " "
+colnames(model_info_table) <- c("term", Qsn_Name_Cont)
+
+model_info_table <- ggtexttable(model_info_table, theme = ttheme("mOrange"), rows = NULL) %>%
+  tab_add_title(text = "Model Fit Estimates", face = "bold", padding = unit(0.1, "line")) %>%
+  tab_add_footnote(text = "*** p < 0.001; ** p < 0.01; * p < 0.05; 99% C.I.", size = 10, face = "italic")
+
+R.2.table <- present_R.2(sig_lin_mod_cats, "mRed", "Model Performance")
+Resid.table <- present_ssr(sig_lin_mod_cats, "mRed", "Model Performance")
+# Models <- ggarrange(model_info_table, R.2.table, nrow = 1, ncol = 2, widths = c(8,4))
+Models <- ggarrange(R.2.table, Resid.table, nrow = 2, ncol = 1)
+# p <- ggarrange(sig_lin_mod_cats_plot, Models, ncol = 1, nrow = 2, heights = c(8, 4))
+
+p <- ggarrange(sig_lin_mod_cats_plot, Models, ncol = 2, nrow = 1, widths = c(8, 4))
+
+annotate_figure(
+  p,
+  top = text_grob("Linear Model for Dependence of Each Category Score on All Other", face = "bold", size = 16)
+)
 
 # Linear Model Dependence of the Catgories with Total Score  # I realized it makes no sense to do this without scaling
 data_net_tot_score <- data_net_score %>%
@@ -67,6 +185,9 @@ data_net_tot_score %>%
   scale_fill_viridis(discrete = TRUE)
 
 # Linear Model Dependence for each item to the Category net score
+
+data_enc_1 <- data_enc %>%
+  bind_cols(data_net_score[,3:ncol(data_net_score)])
 
 lin_mod_items_on_cats <- list()
 Qsn_Name1 <- c("^REC", "^SA", "^PC", "^SS", "^ER", "^ES", "^RES", "^CON")
